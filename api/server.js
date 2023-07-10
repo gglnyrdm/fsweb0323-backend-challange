63623660db53fd9983e17942f0b0f3d376c6a706
Gülin <yardimoglugulin@gmail.com>
2023-07-10 19:57:37 +0300

    backend security measures implemented

--- api/server.js
+++ api/server.js
@@ -2,9 +2,15 @@
 const express = require('express');
 const server = express();
 require('dotenv').config();
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan')
+
 
 //2. global middleware
-
+server.use(helmet());
+server.use(cors());
+server.use(morgan('dev'));
 
 
 //3. routerlar
