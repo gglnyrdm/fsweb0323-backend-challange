ffd37f9b747a77b997fa66a7d8c294733ccaf72a
Gülin <yardimoglugulin@gmail.com>
2023-07-11 22:29:43 +0300

    models updated

--- api/Tweets/tweets-model.js
+++ api/Tweets/tweets-model.js
@@ -1,7 +1,71 @@
-const getAll = () => {
+const db = require('../../data/db-config');
 
+const getAll = () => {
+    return db('Tweets as t')
+            .leftJoin('Users as u', "u.user_id","t.user_id")
+            .leftJoin("Tweet_type as ty", "t.type_id", "ty.type_id")
+            .select("t.tweet_id", 
+                    "t.created_at", 
+                    "t.content",
+                    "u.user_id",
+                    "u.username", 
+                    "ty.type_id",
+                    "ty.type_name"
+                    )
+}
+
+const getById = async (tweet_id) => {
+    const tweet = await db("Tweets as u")
+            .leftJoin('Users as u', "u.user_id","t.user_id")
+            .leftJoin("Tweet_type as ty", "t.type_id", "ty.type_id")
+            .select("t.tweet_id", 
+                    "t.created_at", 
+                    "t.content",
+                    "u.user_id",
+                    "u.username", 
+                    "ty.type_id",
+                    "ty.type_name"
+                    )
+            .where("t.tweet_id",tweet_id)
+            .first()
+    return tweet;
+}
+
+const getByFilter = async(filter) => {
+    return await db('Tweets as t')
+                .leftJoin('Users as u', "u.user_id","t.user_id")
+                .leftJoin("Tweet_type as ty", "t.type_id", "ty.type_id")
+                .select("t.tweet_id", 
+                        "t.created_at", 
+                        "t.content",
+                        "u.user_id",
+                        "u.username", 
+                        "ty.type_id",
+                        "ty.type_name"
+                        )
+                .where(filter)
+}
+
+const create = async(model) => {
+    let [tweet_id] = await db('Tweets').insert(model);
+    return getById(tweet_id);
+}
+
+const remove = async(tweet_id) => {
+    return await db('Tweets').where({tweet_id:tweet_id}).del();
+}
+
+const update = async(tweet_id, tweet) => {
+    await db('Tweets').where({tweet_id:tweet_id}).update(tweet);
+    return getById(tweet_id);
 }
 
 module.exports = {
-    getAll
+    getAll,
+    getByFilter,
+    getById,
+    create,
+    update,
+    remove
+
 }
--- api/Users/users-model.js
+++ api/Users/users-model.js
@@ -1,7 +1,77 @@
-const getAll = () => {
+const db = require('../../data/db-config');
 
+const getAll = () => {
+    return db('Users as u')
+            .leftJoin('Roles as r',"u.role_id","r.role_id")
+            .leftJoin("Follows as f", "u.user_id","f.user_id")
+            .select("u.user_id",
+                    "u.username", 
+                    "u.email", 
+                    "u.first_name", 
+                    "u.lastname", 
+                    "u.created_at",
+                    "r.role_id",
+                    "r.role_name",
+                    "f.following_id",
+                    "f.follower_id")
+}
+
+const getById = async (user_id) => {
+    const user = await db("Users as u")
+    .leftJoin('Roles as r',"u.role_id","r.role_id")
+    .leftJoin("Follows as f", "u.user_id","f.user_id")
+    .select("u.user_id",
+            "u.username", 
+            "u.email",
+             "u.first_name", 
+             "u.lastname", 
+             "u.created_at",
+             "r.role_id",
+             "r.role_name",
+             "f.following_id",
+             "f.follower_id")
+    .where("u.user_id",user_id)
+    .first()
+    return user;
+}
+
+const getByFilter = async(filter) => {
+    return await db('Users as u')
+            .leftJoin('Roles as r',"u.role_id","r.role_id")
+            .leftJoin("Follows as f", "u.user_id","f.user_id")
+            .select("u.user_id",
+            "u.username", 
+            "u.email", 
+            "u.first_name", 
+            "u.lastname", 
+            "u.created_at",
+            "r.role_id",
+            "r.role_name",
+            "f.following_id",
+            "f.follower_id")
+            .where(filter)
+}
+
+const create = async(model) => {
+    let [user_id] = await db('Users').insert(model);
+    return getById(user_id);
+}
+
+const remove = async(user_id) => {
+    return await db('Users').where({user_id:user_id}).del();
+}
+
+const update = async(user_id, user) => {
+    await db('Users').where({user_id:user_id}).update(user);
+    return getById(user_id);
 }
 
 module.exports = {
-    getAll
+    getAll,
+    getByFilter,
+    getById,
+    create,
+    update,
+    remove
+
 }
